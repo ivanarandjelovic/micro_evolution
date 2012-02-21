4d09fe6d2ad8efd6d3fff50d489bdcd9c4e12035
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2012-02-21 15:49:25 +0100

    Initial main class. No implementation yet.

---
+++ micro_evolution/src/org/aivan/microevolution/ME.java
@@ -0,0 +1,18 @@
+package org.aivan.microevolution;
+
+/**
+ * Main class for starting and executing evolutions.
+ * 
+ * @author iarandjelovic
+ * 
+ */
+public class ME {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Not implemented yet!");
+	}
+
+}
